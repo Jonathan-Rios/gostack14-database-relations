6908e8926b6137f9cdbd28c9fdbb0f55419ab93a
Jonathan-Rios <jonathan.riosousa@gmail.com>
2021-02-02 06:59:08 -0300

    chore: Entities-Related

--- src/modules/orders/infra/typeorm/entities/OrdersProducts.ts
+++ src/modules/orders/infra/typeorm/entities/OrdersProducts.ts
@@ -11,23 +11,35 @@ import {
 import Order from '@modules/orders/infra/typeorm/entities/Order';
 import Product from '@modules/products/infra/typeorm/entities/Product';
 
+@Entity('order_products')
 class OrdersProducts {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   order: Order;
 
+  @Column()
   product: Product;
 
+  @ManyToOne(() => Product)
+  @JoinColumn({ name: 'id' })
   product_id: string;
 
+  @ManyToOne(() => Order)
+  @JoinColumn({ name: 'id' })
   order_id: string;
 
+  @Column()
   price: number;
 
+  @Column()
   quantity: number;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
